e452ab5e696a5c76973128760701e9fa0c33d977
Rohan <rohan.raj@nixorcollege.edu.pk>
2025-11-29 11:17:05 +0100

    Exercise Sheet 3 - Task 3 Complete

--- script.js
+++ script.js
@@ -43,3 +43,102 @@ if (productList != null && viewSelect != null) {
 }
 
 
+		
+
+function checkCredentials() {
+	let usernode=document.getElementById('uName');
+	let numbnode=document.getElementById('password');
+	let username=usernode.value;
+	let number=numbnode.value;
+	if(username.length >=5 && /[A-Z]/.test(username)&& /[a-z]/.test(username)){
+		usernode.style.backgroundColor='springgreen';
+		usernode.style.fontWeight='bold';
+		usernode.style.color='white';
+	}else{
+		usernode.style.backgroundColor='red';
+		usernode.style.fontWeight='bold';
+		usernode.style.color='black';
+	}
+	
+	if (number.length >= 10){
+		numbnode.style.backgroundColor='springgreen';
+		numbnode.style.fontWeight='bold';
+		numbnode.style.color='white';
+	}else{
+		numbnode.style.backgroundColor='red';
+		numbnode.style.fontWeight='bold';
+		numbnode.style.color='black';
+	}
+	
+	
+	
+	
+}
+
+function checkRegister() {
+	let usernode=document.getElementById('uName');
+	let numbnode=document.getElementById('password');
+	let cnumbnode=document.getElementById('cPassword');
+	let username=usernode.value;
+	let number=numbnode.value;
+	let number2=cnumbnode.value;
+	if(username.length >=5 && /[A-Z]/.test(username)&& /[a-z]/.test(username)){
+		usernode.style.backgroundColor='springgreen';
+		usernode.style.fontWeight='bold';
+		usernode.style.color='white';
+	}else{
+		usernode.style.backgroundColor='red';
+		usernode.style.fontWeight='bold';
+		usernode.style.color='black';
+	}
+	
+	if (number.length >= 10){
+		numbnode.style.backgroundColor='springgreen';
+		numbnode.style.fontWeight='bold';
+		numbnode.style.color='white';
+	}else{
+		numbnode.style.backgroundColor='red';
+		numbnode.style.fontWeight='bold';
+		numbnode.style.color='black';
+	}
+	if (number === number2){
+		cnumbnode.style.backgroundColor='springgreen';
+		cnumbnode.style.fontWeight='bold';
+		cnumbnode.style.color='white';
+	}else{
+		cnumbnode.style.backgroundColor='red';
+		cnumbnode.style.fontWeight='bold';
+		cnumbnode.style.color='black';
+	}
+}
+
+var collectionList= [];
+
+function displayList(){
+	document.lastChild.lastChild.removeChild(document.lastChild.lastChild.lastChild);
+	let displayList=document.createElement('ol');
+	for (item of collectionList){
+		let displayItem=document.createElement('li');
+		displayItem.innerText=item.toString();
+		displayList.appendChild(displayItem);
+	}
+	document.lastChild.lastChild.appendChild(displayList);
+}
+
+function addToCollection(indicator){
+	purchaseObject={}
+	purchaseObject.name=indicator.firstElementChild.getAttribute('alt');
+	purchaseObject.quantity=indicator.lastElementChild.previousElementSibling.value;
+	purchaseObject.toString= function(){return this.name+ ' quantity: '+this.quantity;};
+	for (item of collectionList){
+		if(item.name===purchaseObject.name){
+			item.quantity=Number(item.quantity)+Number(purchaseObject.quantity);
+			console.log(item.name,item.quantity);
+			displayList();
+			return;
+		}
+	
+	}
+	collectionList.push(purchaseObject);
+	displayList();
+}
